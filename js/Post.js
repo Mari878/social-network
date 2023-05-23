e9469b511b6ba366196c816e1a7bed7193374541
Mari878 <marilovic.milos1994@gmail.com>
2023-05-23 12:41:18 +0200

    s

---
+++ js/Post.js
@@ -0,0 +1,14 @@
+class Post {
+	post_id = '';
+	post_conetnt = '';
+	user_id = '';
+	likes = '';
+	api_url = 'https://64423d3476540ce225874f8e.mockapi.io';
+
+	async create(post_content) {
+		let session = new Session();
+		session_id = session.getSession();
+
+		let data = 
+	}
+}
